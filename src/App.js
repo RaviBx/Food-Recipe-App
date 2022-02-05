c72affc467964584dd7ef993bf100e1cd26cb3c3
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-05 20:40:06 +0530

    Added EDAMAM Api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import './key';
 
 function App() {
+
+
+  var url = `https://api.edamam.com/search?q=chicken&
+  app_id=2faec28c&app_key=726c2a74df0895a29f0571a1e988afab&from=0&to=3&calories=591-722&health=alcohol-free`;
+
   return (
     <div className="App">
      
 
 
-     
+
     </div>
   );
 }
---
+++ src/key.js
@@ -0,0 +1,3 @@
+const YOUR_APP_ID = "2faec28c";
+
+const YOUR_APP_KEY = "726c2a74df0895a29f0571a1e988afab";
