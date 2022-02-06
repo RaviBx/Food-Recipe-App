7c99182d9f70609105be0efaff0af07c92b945ca
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-06 16:38:28 +0530

    Set getRecipe function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import './key';
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
+import './component/RecipeTile';
+import RecipeTile from './component/RecipeTile';
 
 function App() {
 
 
   const [query, setquery] = useState("");
+
+  const [recipes, setrecipes] = useState([]);
 
   const YOUR_APP_ID = "2faec28c";
 
@@ -18,22 +22,24 @@ function App() {
 
 
 
+
 async function getRecipes(){
 
   
      var result = await Axios.get(url);
+     setrecipes(result.data.hits)
+
      console.log(result.data);
 
 }
 
 
-const onSubmit = (e) => {
+const sendData = (e) => {
 
-  e.preventDeafault();
+  e.preventDefault();
   getRecipes();
  
-
-}
+};
 
 
   return (
@@ -42,10 +48,31 @@ const onSubmit = (e) => {
 
       <h1 > Food Recipe Plaza </h1>
 
-      <form className='app_searchForm' onSubmit={onSubmit}>
-        <input type="text" className='app_input' placeholder='Enter Ingredients' value={query} onChange={(e) => setquery(e.target.value)}/>
+      <form className='app_searchForm' onSubmit={sendData}>
+
+        <input type="text" className='app_input' placeholder='Enter Ingredients' value={query} 
+        
+        onChange={(e) => setquery(e.target.value)}/>
+
+
         <input type="submit" className='app_submit' value="Search" />
+
       </form>
+
+
+      <div>
+       
+        {recipes.map(recipe => {
+
+            
+            return <RecipeTile recipe={recipe}/>
+              
+            
+
+        })}
+
+      </div>
+
 
     </div>
   );
--- src/component/RecipeTile.js
+++ src/component/RecipeTile.js
@@ -1,3 +1,17 @@
-import React from "react";
+import React from 'react';
+
+export default function RecipeTile({ recipe }) {
+  return(
+      
+    <div className='recipeTile'>
+    
+                <p>{recipe["recipe"]["label"]}</p>
+
+   
+    </div>
+
+  );
+}
+
 
 
